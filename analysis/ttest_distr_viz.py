b7a2b85dd8af3f1bf0651a2ac41af10e232f295c
houchenst <trevormhouchens@gmail.com>
2020-05-06 22:54:44 -0400

    all distribution images should be here now

--- analysis/ttest_distr_viz.py
+++ analysis/ttest_distr_viz.py
@@ -4,55 +4,113 @@ import pandas as pd
 import os
 from scipy.stats import norm
 
+threshold = 200000
+hard_housing = 200000
+hard_pop = 1000
+hard_thresh = False
+drop_percentile = 100
+pop = False
+use_zeros = True
+
+pop_low = 500000
+pop_high = 10000000
+
+num_bins = 100
+
 
 if __name__ == "__main__":
-    df = pd.read_csv("../data/joined_agg.csv")
-    df= df[""]
+    df = pd.read_csv("./data/joined_agg.csv")
+    df= df[["HousingPrice", "NumIncidents", "Population", "PopDensity"]]
+    df["GVRate"] = (df["NumIncidents"] * 1000) / (df["Population"]*4.25)
     df = df.dropna(axis=0)
 
-    df = df.loc[df["DATE"].map(dateInRange)]
+    df = df[df["Population"] > pop_low]
+    df = df[df["Population"] < pop_high]
 
-    temps = np.array(df["TAVG"])
-    # print(temps)
+    med_pop = np.median(np.array(df["PopDensity"]))
+    med_housing = np.median(np.array(df["HousingPrice"]))
+    print(f"Median Housing Price - {med_housing}")
+    print(f"Median Population Density - {med_pop}")
 
-    num_samples = temps.shape[0]
-    num_bins = int(np.max(temps) - np.min(temps) + 1)
+    if pop and not hard_thresh:
+        threshold = med_pop
+    elif not pop and not hard_thresh:
+        threshold = med_housing
+    elif pop and hard_thresh:
+        threshold = hard_pop
+    elif not pop and hard_thresh:
+        threshold = hard_housing
 
-    hist, bin_edges = np.histogram(temps, bins=num_bins)
-    # normalize
-    hist = hist/num_samples
+    field = "PopDensity" if pop else "HousingPrice"
 
-    # gaussian distribution estimate
-    norm_x = np.linspace(np.min(temps), np.max(temps), num=1000)
-    mu = np.mean(temps)
-    sigma = np.std(temps)
-    norm_y = norm.pdf(norm_x, mu, sigma)
 
-    ax = plt.subplot(111)
-    ax.bar(bin_edges[:-1], hist)
-    ax.plot(norm_x, norm_y, color="g")
-    ax.set_title("Probability Distribution of Average Daily Temperature in Burlington, VT")
+    low = df[df[field] < threshold]
+    high = df[df[field] >= threshold]
+    low_rates = np.array(low["GVRate"])
+    high_rates = np.array(high["GVRate"])
+
+    hist_range = (0, np.percentile(np.hstack((low_rates, high_rates)), drop_percentile))
+    # update rates arrays to drop data outside of range
+    low = low[low["GVRate"] < hist_range[1]]
+    full_low = np.array(low["GVRate"])
+    low = low[low["GVRate"] > 0]
+    low_rates = np.array(low["GVRate"])
+    high = high[high["GVRate"] < hist_range[1]]
+    full_high = np.array(high["GVRate"])
+    high = high[high["GVRate"] > 0]
+    high_rates = np.array(high["GVRate"])
+    
+    # num_bins = int((low_rates.shape[0] + high_rates.shape[0]) / 200)
+
+    field_title = "Population Density" if pop else "Housing Price"
+
+
+    fig, ax = plt.subplots(figsize=(15,9))
+    plt.xlim(hist_range)
+    ax.set_title(f'Distribution of Gun Violence Rates For Cities with Populations of {"{:,}".format(pop_low)}-{"{:,}".format(pop_high)} \n By High and Low {field_title}')
     ax.set_ylabel("Probability Density")
-    ax.set_xlabel("Temperature (Fahrenheit)")
-    plt.savefig("btv_temp_distr.png")
+    ax.set_xlabel("Gun Violence Rates (Number of Incidents per 1,000 People per Year)")
+    # ax.ylim()
+    str_thresh = "{:,}".format(int(threshold))
+    if use_zeros:
+        to_plot = [(low_rates, "r", f'{field_title} < {str_thresh}', full_low), (high_rates, "b", f'{field_title} > {str_thresh}', full_high)]
+    else:
+        to_plot = [(low_rates, "r", f'{field_title} < {str_thresh}'), (high_rates, "b", f'{field_title} > {str_thresh}')]
+    
 
+    for i, x in enumerate(to_plot):
+        rates = x[0]
+        c = x[1]
+        label = x[2]
+        if use_zeros:
+            full_rates = x[3]
+        hist, bin_edges = np.histogram(rates, bins=num_bins, range=hist_range, density=False)
 
-    # create shifted vars
-    shift = 10
-    s_norm_x = np.linspace(np.min(temps), np.max(temps)+shift, num=1100)
-    s_mu = mu + shift
-    s_norm_y = norm.pdf(s_norm_x, s_mu, sigma)
+        if use_zeros:
+            hist = hist/(full_rates.shape[0]*(bin_edges[1]-bin_edges[0]))
+        else:
+            hist = hist/(rates.shape[0]*(bin_edges[1]-bin_edges[0]))
 
-    s_bin_edges = bin_edges + shift
+        # gaussian distribution estimate
+        norm_x = np.linspace(hist_range[0], hist_range[1], num=1000)
+        if use_zeros:
+            mu = np.mean(full_rates)
+            sigma = np.std(full_rates)
+        else:
+            mu = np.mean(rates)
+            sigma = np.std(rates)
+        label += f"\nMean: {round(mu, 4)}\n Standard Deviation: {round(sigma, 4)}" 
+        norm_y = norm.pdf(norm_x, mu, sigma)
 
+        bar_width = (hist_range[1]-hist_range[0])/num_bins/2
+        ax.bar(bin_edges[:-1]+bar_width*i, hist, color=c, alpha=0.5, width=bar_width)
+        ax.plot(norm_x, norm_y, color=c, label=label)
+    
+    ax.legend()
+    
+    # plt.show()
 
-    ax.cla()
-    ax.bar(s_bin_edges[:-1], hist)
-    ax.plot(s_norm_x, s_norm_y, color="g")
-    ax.set_title(f"PDF of Average Daily Temperature in Burlington, VT (Shifted {shift} Degrees)")
-    ax.set_ylabel("Probability Density")
-    ax.set_xlabel("Temperature (Fahrenheit)")
-    plt.savefig("btv_temp_distr_shift.png")
+    plt.savefig(f'./analysis/output/distribution/distribution_{"popdensity" if pop else "housing"}_{pop_low}_{pop_high}.png')
 
 
 
